329765799ad1467a3ef71ae5c82de7e59da3319a
Deon Thomas <deon.thomas.gy@gmail.com>
2016-11-04 22:03:16 -0400

    updated migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('first_name');
             $table->string('last_name');
+            $table->string('landline');
+            $table->string('cellphone');
             $table->string('email')->unique();
             $table->string('business_name');
             $table->integer('location_id')->unsigned();
--- database/migrations/2016_11_05_001527_create_orders_table.php
+++ database/migrations/2016_11_05_001527_create_orders_table.php
@@ -18,6 +18,7 @@ class CreateOrdersTable extends Migration
             $table->integer('produce_id');
             $table->integer('quantity');
             $table->integer('location_id')->unsigned();
+            $table->boolean('active');
         });
     }
 
