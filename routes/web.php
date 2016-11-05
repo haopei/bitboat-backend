2cdb5447ee702a7e8c48438dcbc156c5106543cb
Deon Thomas <deon.thomas.gy@gmail.com>
2016-11-05 15:41:01 -0400

    rename route

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::get('/api/v1/user/login', 'UsersController@login');
 Route::get('/api/v1/user/{id}', 'UsersController@UserByIdGet');
 
 Route::get('/api/v1/produce/all', 'ProducesController@index');
-Route::get('/api/v1/produce/byId/{id}', 'ProducesController@ByIdGet');
+Route::get('/api/v1/produce/{id}', 'ProducesController@ByIdGet');
 Route::get('/api/v1/produce/byUserId/{id}', 'ProducesController@ByUserIdGet');
 
 Route::get('/api/v1/buyers/all', 'UsersController@BuyersAllGet');
