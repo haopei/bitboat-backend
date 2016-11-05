1c09f10d6a7e5f4d7404e64442a595e2700ac632
Deon Thomas <deon.thomas.gy@gmail.com>
2016-11-05 10:47:14 -0400

    update seeds

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,10 +15,32 @@ class UsersTableSeeder extends Seeder
         DB::table('roles')->insert(['name' => 'supplier']);
         DB::table('roles')->insert(['name' => 'producer']);
 
-        for ($i = 1; $i <= 20; $i++) {
+        $produces = array(
+            array('name' =>  'apples', 'image_url' => 'img/apples.jpg', 'description' => ''),
+            array('name' =>  'avocados', 'image_url' => 'img/avocados.jpg', 'description' => ''),
+            array('name' =>  'bora', 'image_url' => 'img/bora.jpg', 'description' => ''),
+            array('name' =>  'cabbages', 'image_url' => 'img/cabbages.jpg', 'description' => ''),
+            array('name' =>  'carrots', 'image_url' => 'img/carrots.jpg', 'description' => ''),
+            array('name' =>  'cucumbers', 'image_url' => 'img/cucumbers.jpg', 'description' => ''),
+            array('name' =>  'eggplants', 'image_url' => 'img/eggplants.jpg', 'description' => ''),
+            array('name' =>  'eggplants2', 'image_url' => 'img/eggplants2.jpg', 'description' => ''),
+            array('name' =>  'grapes', 'image_url' => 'img/grapes.jpg', 'description' => ''),
+            array('name' =>  'lemons', 'image_url' => 'img/lemons.jpg', 'description' => ''),
+            array('name' =>  'lettuces', 'image_url' => 'img/lettuces.jpg', 'description' => ''),
+            array('name' =>  'mangoes-354062_1280', 'image_url' => 'img/mangoes-354062_1280.jpg', 'description' => ''),
+            array('name' =>  'onions', 'image_url' => 'img/onions.jpg', 'description' => ''),
+            array('name' =>  'ornges', 'image_url' => 'img/ornges.jpg', 'description' => ''),
+            array('name' =>  'papayas', 'image_url' => 'img/papayas.jpg', 'description' => ''),
+            array('name' =>  'papayas2', 'image_url' => 'img/papayas2.jpg', 'description' => ''),
+            array('name' =>  'potatoes', 'image_url' => 'img/potatoes.jpg', 'description' => ''),
+            array('name' =>  'pumpkins', 'image_url' => 'img/pumpkins.jpg', 'description' => ''),
+            array('name' =>  'tomatoes', 'image_url' => 'img/tomatoes.jpg', 'description' => '')
+        );
+
+        foreach ($produces as $item) {
             DB::table('produces')->insert([
-                'name' => "$faker->word $faker->word",
-                'image_url' => $faker->imageUrl(640, 480),
+                'name' => $item['name'],
+                'image_url' => $item['image_url'],
                 'description' => $faker->paragraph(2)
             ]);
         }
