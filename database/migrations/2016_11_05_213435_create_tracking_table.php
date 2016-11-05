e6c0f6334d16be1e2b93311662822b4480b7d78c
Deon Thomas <deon.thomas.gy@gmail.com>
2016-11-05 17:38:40 -0400

    added tracking

---
+++ database/migrations/2016_11_05_213435_create_tracking_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrackingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tracking', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('status');
+            $table->integer('producer_location_id')->unsigned();
+            $table->integer('delivery_location_id')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
