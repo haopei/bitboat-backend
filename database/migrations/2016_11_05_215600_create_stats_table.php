73380aafc9bafb5578bb60c5201d36c2a20f6c91
Deon Thomas <deon.thomas.gy@gmail.com>
2016-11-05 17:59:53 -0400

    added new migration

---
+++ database/migrations/2016_11_05_215600_create_stats_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stats', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('produce_id')->unsigned();
+            $table->integer('produced')->unsigned();
+            $table->integer('demanded')->unsigned();
+            $table->integer('bid_avg')->unsigned();
+            $table->integer('order_avg')->unsigned();
+            $table->integer('month');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
