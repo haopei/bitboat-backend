e57a3650d139977cd92c4cf402b095b3af1ef451
Deon Thomas <deon.thomas.gy@gmail.com>
2016-11-04 22:05:02 -0400

    add Models..

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name', 'last_name','cellphone', 'landline', 'business_name', 'email', 'password',
     ];
 
     /**
